cc768c87c11b3baf24532dae9d62aaf3faaaf416
makelele29 <jacastillo.ugr@gmail.com>
2016-10-22 19:08:04 +0200

    Creado gruntfile para documentación de la libreria

--- instalacion.sh
+++ instalacion.sh
@@ -2,15 +2,16 @@
 sudo apt-get update
 sudo apt-get install lamp-server^
 sudo apt install node-jade node-express-generator
-mkdir ranking
-cd ranking
+mkdir Web
+cd Web
 express
-mv -f ../package.json ../app.js .
-mv -f ../views/* views/
-mv -f ../public/* public/
-rm -r ../views ../public
+cp -f ../Ranking/package.json ../Ranking/app.js ../Ranking/Gruntfile.js .
+cp -f ../Ranking/views/* views/
+
+
 echo "Añadidas las carpetas"
 cd . && npm install
-npm install express-session
+npm install express-session --save-dev
+npm install grunt-docco --save-dev
+grunt docco
 DEBUG=my-application ./bin/www
-
