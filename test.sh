507d7f3cf5fe33288cd54cb52f3832e04ec87250
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 03:42:05 +0200

    añadido instalacion global de express

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ export TESTSUITE_PASSWORD=root
 export TESTSUITE_URL=http://127.0.0.1:8000
 export CI_MODE=test
 mysql -uroot  < ranking.sql
+npm install -g express
 mkdir Web
 cd Web
-npm install -g express
 express
 cp -Rf ../Ranking/* .
 echo "Añadidas las carpetas"
