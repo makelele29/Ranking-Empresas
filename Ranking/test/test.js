c4f79119a38eaea2c4936adb7ee4b1891bdd30ac
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 03:16:52 +0200

    Importando BD desde script

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -12,12 +12,19 @@ function BD(){
   return conexion;
 }
 describe("Prueba de test",function(){
+  it("Insertar",function(done){
+    var bd=BD();
+    bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (111,'Javier','Castillo','ja@ja','111')",function (error){
+      assert.ok(!error,"Hubo un error al registar al usuario");
+      bd.end(done);
+    });
+  });
   it("Login",function(done){
 
     var bd=BD();
 
     bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=111 AND clave=111",function(error,resultado,fila){
-     
+
       assert.ok(!error,"Error en el select de usuarios");
       assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
       bd.end(done);
