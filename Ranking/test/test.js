19c0d5de2cded9e8fd395b154845d1dfe9e10ab6
makelele29 <jacastillo.ugr@gmail.com>
2016-10-24 00:44:46 +0200

    Probando otro test

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -14,18 +14,22 @@ function BD(){
 // Función para las pruebas con la base de datos
 describe("Prueba Test",function(){
   // Función para añadir un nuevo alumno
-  it("Insertar un alumno",function(){
+  it("Insertar un alumno",function(done){
     var bd=BD();
+    bd.connect(done);
     bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
       assert.ok(!error,"Hubo un error al registar al usuario");
+      bd.end(done);
     });
   });
   // Función para comprobar que el usuario esta loqueado
-  it("Iniciar sesion con un usuario ya registrado",function(){
+  it("Iniciar sesion con un usuario ya registrado",function(done){
     var bd=BD();
+    bd.connect(done);
     bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=22 AND clave=222",function(error,resultado,fila){
       assert.ok(!error,"Error en el select de usuarios");
       assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
+      bd.end(done);
     });
   });
 });
