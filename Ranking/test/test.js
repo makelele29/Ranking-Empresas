a1f7a7d551bc863ccf7c5e2bb58f28c0ac39bed0
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 16:59:15 +0200

    Comentado test.js

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -10,13 +10,17 @@ function BD(){
   });
   return conexion;
 }
+//-----------------------------------------------------------------------------------------------------------------------
+// Función para las pruebas con la base de datos
 describe("Prueba Test",function(){
+  // Función para añadir un nuevo alumno
   it("Insertar un alumno",function(){
     var bd=BD();
     bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
       assert.ok(!error,"Hubo un error al registar al usuario");
     });
   });
+  // Función para comprobar que el usuario esta loqueado
   it("Iniciar sesion con un usuario ya registrado",function(){
     var bd=BD();
     bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
