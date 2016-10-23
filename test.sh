2ac222c052a86811b865cce4b704b653676985d6
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 04:04:55 +0200

    Probando a travis

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-export TESTSUITE_USER=root
-export TESTSUITE_PASSWORD=root
-export TESTSUITE_URL=http://127.0.0.1:8000
-export CI_MODE=test
-mysql -uroot  < ranking.sql
+mysql  < ranking.sql
 mkdir Web
 cd Web
 cp -Rf ../Ranking/* .
