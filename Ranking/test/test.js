2f108371a35ceec46eb5c888a020d62bf3c71169
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 05:30:10 +0200

    Test definitivo

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -10,26 +10,18 @@ function BD(){
   });
   return conexion;
 }
-
-  describe("Prueba Test",function(){
-    it("Insertar un alumno",function(){
-      var bd=BD();
-      bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
-        assert.ok(!error,"Hubo un error al registar al usuario");
-
-
-      });
-    });
-    it("Iniciar sesion con un usuario ya registrado",function(){
-
-      var bd=BD();
-
-      bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
-
-        assert.ok(!error,"Error en el select de usuarios");
-        assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
-
-      });
-
+describe("Prueba Test",function(){
+  it("Insertar un alumno",function(){
+    var bd=BD();
+    bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
+      assert.ok(!error,"Hubo un error al registar al usuario");
     });
   });
+  it("Iniciar sesion con un usuario ya registrado",function(){
+    var bd=BD();
+    bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
+      assert.ok(!error,"Error en el select de usuarios");
+      assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
+    });
+  });
+});
