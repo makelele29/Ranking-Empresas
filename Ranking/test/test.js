32f6a07818e059f43667bff34e6cb2714401be21
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 05:23:19 +0200

    Arreglado test.js

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -16,21 +16,20 @@ function BD(){
       var bd=BD();
       bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
         assert.ok(!error,"Hubo un error al registar al usuario");
-        it("Login",function(){
-          it("Iniciar sesion con un usuario ya registrado",function(done){
 
-            var bd=BD();
-
-            bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
-
-              assert.ok(!error,"Error en el select de usuarios");
-              assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
-              bd.end(done);
-            });
-
-          });
-        });
 
       });
+    });
+    it("Iniciar sesion con un usuario ya registrado",function(done){
+
+      var bd=BD();
+
+      bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
+
+        assert.ok(!error,"Error en el select de usuarios");
+        assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
+        bd.end(done);
+      });
+
     });
   });
