14ea6895dc99f24ac6fa84da54c5821a044c8360
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 03:51:11 +0200

    Añadiendo carpetas necesarias en Ranking

--- test.sh
+++ test.sh
@@ -4,10 +4,8 @@ export TESTSUITE_PASSWORD=root
 export TESTSUITE_URL=http://127.0.0.1:8000
 export CI_MODE=test
 mysql -uroot  < ranking.sql
-sudo apt-get install node-express-generator
 mkdir Web
 cd Web
-express
 cp -Rf ../Ranking/* .
 echo "Añadidas las carpetas"
 cd . && npm install
