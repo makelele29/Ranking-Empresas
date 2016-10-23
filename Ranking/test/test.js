316ff32e282a741db593d6891a488d8c4bd15266
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 05:07:49 +0200

    Probando nuevo test.js

--- Ranking/test/test.js
+++ Ranking/test/test.js
@@ -11,30 +11,27 @@ function BD(){
   return conexion;
 }
 
-  describe("Insertar",function(){
+  describe("Prueba Test",function(){
     it("Insertar un alumno",function(done){
       var bd=BD();
       bd.query("INSERT INTO  USUARIOS (DNI,nombre,apellidos,email,clave) values (222,'Javier','Castillo','ja@ja','222')",function (error){
         assert.ok(!error,"Hubo un error al registar al usuario");
         bd.end(done);
+        it("Login",function(){
+          it("Iniciar sesion con un usuario ya registrado",function(done){
+
+            var bd=BD();
+
+            bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=222 AND clave=222",function(error,resultado,fila){
+
+              assert.ok(!error,"Error en el select de usuarios");
+              assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
+              bd.end(done);
+            });
+
+          });
+        });
+
       });
-    });
-  });
-
-
-
-
-  describe("Login",function(){
-    it("Iniciar sesion con un usuario ya registrado",function(done){
-
-      var bd=BD();
-
-      bd.query("SELECT DNI,nombre FROM USUARIOS WHERE DNI=22254 AND clave=222",function(error,resultado,fila){
-
-        assert.ok(!error,"Error en el select de usuarios");
-        assert.notEqual(resultado.length,0,"El usuario introducido no es correcto");
-        bd.end(done);
-      });
-
     });
   });
