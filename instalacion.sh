8e414391c17610844005b2813e76b11e53a33640
makelele29 <jacastillo.ugr@gmail.com>
2016-10-23 16:52:12 +0200

    Añadido en gitignore carpeta creada por grunt

--- instalacion.sh
+++ instalacion.sh
@@ -2,13 +2,8 @@
 sudo apt-get update
 sudo apt-get install lamp-server^
 mysql -u root -p  < ranking.sql
-sudo apt install node-jade node-express-generator
-mkdir Web
-cd Web
-express
-cp -Rf ../Ranking/* .
-echo "Añadidas las carpetas"
-cd . && npm install
+cd Ranking
+npm install
 npm install -g grunt-docco
 grunt docco
 
